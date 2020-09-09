e38002676a0ca4d4f7933e694ae7593186708585
Caleb Jackson <jacksoncaleb.calebjackson@gmail.com>
2020-09-09 12:24:37 -0400

    router probs -add NavBar next

--- src/Components/Routes/HomePage.js
+++ src/Components/Routes/HomePage.js
@@ -1,11 +1,14 @@
-import React, { Component } from 'react'
-
+import React, {
+    Component
+} from 'react'
+//add a navBar next
 export default class HomePage extends Component {
     render() {
-        return (
-            <div>
-                <h1>THIS IS THE HOME PAGE</h1>
-            </div>
+        return ( <
+            div >
+            <
+            h1 > THIS IS THE HOME PAGE < /h1> <
+            /div>
         )
     }
 }
